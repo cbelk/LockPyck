0cf0c1926a613847844ef357d92414cab24d9361
Christian Belk <cbelk88@gmail.com>
2016-03-16 06:29:32 -0400

    added functionality to provide frequency count and sort freak sheets

--- PyckMaster/freak_roundup.py
+++ PyckMaster/freak_roundup.py
@@ -4,7 +4,15 @@ import string
 import csv
 import os
 import shutil
+import operator
 
+lp = '***/LockPyck/'  #replace *** with path to LockPyck
+
+# This function takes a sequence list and a character. If the sequence is empty,
+# the character type representation {D igit, L etter, S pecial, W hitespace} is
+# added to the list with a count of 1. Otherwise, the previously added char type
+# rep is checked to see if it matches the current type. If it does it's count is
+# incremented, else it's type rep is added to the list with a count of 1.
 def updateSeq (sequ, char):
     if char in string.letters:
         if len(sequ) > 0:
@@ -48,13 +56,15 @@ def updateSeq (sequ, char):
             sequ.append(1)
     return
 
+# This function takes a sequence string argument. If the SeqFreak file contains
+# the sequence it's count is incremented by 1, else its added with a count of 1.
 def updateSeqFreak (sequ):
-    seqfreakfile = '***/LockPyck/FreakSheets/Seq.freak' #replace *** with path to LockPyck
-    seqfreakwrite = seqfreakfile + '~'
+    seqfreakin = lp + 'FreakSheets/Seq.freak'
+    seqfreakout = seqfreakin + '~'
     found = False
-    writer = open(seqfreakwrite, 'a+')
-    if os.stat(seqfreakfile).st_size > 0:
-        with open(seqfreakfile, 'r') as seqfreak:
+    writer = open(seqfreakout, 'a+')
+    if os.path.isfile(seqfreakin) and os.stat(seqfreakin).st_size > 0:
+        with open(seqfreakin, 'r') as seqfreak:
             reader = csv.reader(seqfreak)
             for row in reader:
                 if row:
@@ -63,27 +73,30 @@ def updateSeqFreak (sequ):
                         c = row[1].strip('\n')
                         count = int(c) + 1
                         row[1] = str(count) + '\n'
-                        writer.write(row[0] + ',' + str(row[1]) + '\n')
+                        writer.write(row[0] + ',' + row[1])
                     else:
-                        writer.write(row[0] + ',' + str(row[1]) + '\n')
+                        a = row[1].strip('\n')
+                        writer.write(row[0] + ',' + a + '\n')
         seqfreak.close()
     if not found:
         writer.write(sequ + ',1\n')
     writer.close()
-    shutil.move(seqfreakwrite, seqfreakfile)
+    shutil.move(seqfreakout, seqfreakin)
     return
 
+# This function takes the path to a freakfile as an argument. It sums the frequencies in
+# the file, and adds the count to the file.
 def createFreakCount (freakfile):
+    print '[+] Counting the freaks in ' + freakfile + '\n'
     count = 0
     with open(freakfile, 'r') as freakin:
         reader = csv.reader(freakin)
         for row in reader:
-            print row
             if len(row) == 2:
                 count += int(row[1].strip('\n'))
     freakin.close()
-    with open(freakfile, 'r') as freakin, open (freakfile + '~', 'a+') as freakout:
-        freakout.write('count,' + str(count) + '\n')
+    with open(freakfile, 'r') as freakin, open(freakfile + '~', 'a+') as freakout:              #Just add count to end since sortaFreaky
+        freakout.write('count,' + str(count) + '\n')                                            #will move to top
         for line in freakin:
             freakout.write(line)
     freakin.close()
@@ -91,7 +104,24 @@ def createFreakCount (freakfile):
     shutil.move(freakfile + '~', freakfile)
     return
 
-with open('***') as passlist:  #replace *** with path to password list
+# This function takes the path to a freakfile as an argument. It then sorts the freakfile
+# in descending order of frequencies.
+def sortaFreaky (freakfile):
+    print '[+] Sorting the freaks in ' + freakfile + '\n'
+    with open(freakfile, 'r') as freakin, open(freakfile + '~', 'a+') as freakout:
+        reader = csv.reader(freakin)
+        sortedFreak = sorted(reader, key=lambda ro: int(ro[1]), reverse=True)
+        for row in sortedFreak:
+#            print row[0] + ',' + row[1] + '\n'
+            freakout.write(row[0] + ',' + row[1] + '\n')
+    freakin.close()
+    freakout.close()
+    shutil.move(freakfile + '~', freakfile)
+
+print '[+] Starting the freak roundup...\n'
+
+pl = '***'  #replace *** with path to password list
+with open(pl) as passlist:
     for pswd in passlist:
         seq = []
         pswd = pswd.strip('\n')
@@ -101,4 +131,5 @@ with open('***') as passlist:  #replace *** with path to password list
         for c in seq:
             seqString += str(c)
         updateSeqFreak(seqString)
-createFreakCount('***/LockPyck/FreakSheets/Seq.freak')  #replace *** with path to LockPyck
+createFreakCount(lp + 'FreakSheets/Seq.freak')
+sortaFreaky(lp + 'FreakSheets/Seq.freak')
