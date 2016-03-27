cf5cebb5c2263fe1fefbbde78615823887d381f7
Christian Belk <cbelk88@gmail.com>
2016-03-26 20:46:18 -0400

    fixed spacing in comments and removed that script to run that brokedown list.

--- LockPyck/PyckMaster/graveyard.py
+++ LockPyck/PyckMaster/graveyard.py
@@ -1,21 +1,21 @@
 #########################################################################################
-#											#
-#    LockPyck -- A Password Cracker Powered By Probabilistic Context free grammars	#
-#    Copyright (C) 2016  Christian Belk -- cbelk88@gmail.com				#
-#											#
-#    This program is free software: you can redistribute it and/or modify		#
-#    it under the terms of the GNU General Public License as published by		#
-#    the Free Software Foundation, either version 3 of the License, or			#
-#    (at your option) any later version.						#
-#											#
-#    This program is distributed in the hope that it will be useful,			#
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of			#
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			#
-#    GNU General Public License for more details.					#
-#											#
-#    You should have received a copy of the GNU General Public License			#
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.   		#
-#											#
+#                                                                                       #
+#    LockPyck -- A Password Cracker Powered By Probabilistic Context free grammars      #
+#    Copyright (C) 2016  Christian Belk -- cbelk88@gmail.com                            #
+#                                                                                       #
+#    This program is free software: you can redistribute it and/or modify               #
+#    it under the terms of the GNU General Public License as published by               #
+#    the Free Software Foundation, either version 3 of the License, or                  #
+#    (at your option) any later version.                                                #
+#                                                                                       #
+#    This program is distributed in the hope that it will be useful,                    #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+#    GNU General Public License for more details.                                       #
+#                                                                                       #
+#    You should have received a copy of the GNU General Public License                  #
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.              #
+#                                                                                       #
 #########################################################################################
 #
 ######################### The graveyard of inefficient freaks #########################
--- LockPyck/PyckMaster/runnin_crackstation.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-import os
-import super_freak
-
-crackstation_dir = '' #put in the absolute path to the p1 directory you have
-for passlist in os.listdir(crackstation_dir):
-    super_freak.main(os.path.join(crackstation_dir, passlist))
